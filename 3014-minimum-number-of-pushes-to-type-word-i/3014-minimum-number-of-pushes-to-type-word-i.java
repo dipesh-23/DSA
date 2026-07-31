7bde1e571ac9ce95d34d5537003007ed23a4b6be
Dipesh Shitkar <dipeshshitkar2301@gmail.com>
2026-07-31 19:57:35 +0530

    Time: 0 ms (100%), Space: 42.9 MB (95.65%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int ans =0;
+
+        for(int i=0; i<word.length(); i++){
+            if(i<=7){
+                ans++;
+            }else if(i<=15){
+                ans+=2;
+            }else if(i<=23){
+                ans+= 3;
+            }else{
+                ans+= 4;
+            }
+        }
+
+        return ans;
+    }
+}
