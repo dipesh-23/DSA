52acb2e6e10b83f8e76796616959b4732fac20cb
Dipesh Shitkar <dipeshshitkar2301@gmail.com>
2026-08-02 20:47:18 +0530

    Time: 2 ms (83.52%), Space: 45.6 MB (50.61%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,8 +1,7 @@
 class Solution {
-
     public static int f(int i, int j,List<List<Integer>> t,int[][] dp){
 
-        if(i == t.size() - 1){
+        if(i == t.size()-1){
             return t.get(i).get(j);
         }
 
@@ -10,42 +9,22 @@ class Solution {
             return dp[i][j];
         }
 
-        int down = t.get(i).get(j) + f(i+1,j,t,dp);
-        int diag = t.get(i).get(j) + f(i+1,j+1,t,dp);
+        int left = t.get(i).get(j) + f(i+1,j,t,dp);
+        int right = t.get(i).get(j) + f(i+1,j+1,t,dp);
 
-        return dp[i][j] = Math.min(down,diag);
-
+        return dp[i][j] =Math.min(left,right);
     }
     public int minimumTotal(List<List<Integer>> t) {
-        //Here we staarted memoizaton from 0,0 why because if we start from last there are n states to  maintain for sum so why cant we start from single state 0,0
-        int m = t.size();
-        int n = m;
-        int[][] dp = new int[m][n];
+        
+        int n = t.size();
+        int m = t.get(n-1).size();
 
-        //Tabulation is exactly opp of what u have done in memoization.
-        //base case is for row n-1
+        int[][] dp = new int[n][m];
 
-        int[] prev = new int[n];
         for(int i=0; i<n; i++){
-            prev[i] = t.get(n-1).get(i);
+            Arrays.fill(dp[i],Integer.MAX_VALUE);
         }
 
-        for(int i=n-2; i>=0; i--){
-            int[] temp = new int[n];
-            for(int j=0; j<=i; j++){
-
-                temp[j] = t.get(i).get(j)+Math.min(prev[j],prev[j+1]);
-            }
-
-            prev = temp;
-        }
-
-        return prev[0];
-        //space optimize using the prev and temp arr;
-
-        // for(int i=0; i<m; i++){
-        //     Arrays.fill(dp[i],Integer.MAX_VALUE);
-        // }
-        // return f(0,0,t,dp);
+        return f(0,0,t,dp);
     }
 }
