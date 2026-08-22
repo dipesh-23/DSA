f6674cf7310387768dc316ef331ad22bae5727e1
Dipesh Shitkar <dipeshshitkar2301@gmail.com>
2026-08-23 00:30:54 +0530

    Time: 3 ms (86.36%), Space: 52.4 MB (6.18%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int candy(int[] nums) {
+        
+        int n = nums.length;
+        int[] arr = new int[n];
+
+        Arrays.fill(arr,1);
+        int ans = 0;
+
+        for(int i=0; i<n-1; i++){
+            if(nums[i+1] > nums[i]){
+                arr[i+1] = arr[i]+1;
+            }
+        }
+
+        for(int i=n-1; i>0; i--){
+            if(nums[i-1] > nums[i]){
+                arr[i-1] = Math.max(arr[i-1],arr[i]+1);
+            }
+        }
+        
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum+=arr[i];
+        }
+
+        return sum;
+    }
+}
