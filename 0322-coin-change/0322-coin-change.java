4ec5cd0c1e90dee14918b15a4c92c60c0fbf0af2
Dipesh Shitkar <dipeshshitkar2301@gmail.com>
2026-09-12 23:53:15 +0530

    Time: 19 ms (39.54%), Space: 47.1 MB (21.05%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -25,7 +25,35 @@ class Solution {
             Arrays.fill(dp[i],-1);
         }
 
-        int ans = f(0,coins,amount,dp);
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 0;
+        }
+
+        // Fill row 0 (using only coins[0])
+        for (int j = 1; j <= amount; j++) {
+            if (j % coins[0] == 0) {
+                dp[0][j] = j / coins[0];
+            } else {
+                dp[0][j] = (int) 1e9;
+            }
+        }
+
+        for(int i=1; i<n; i++){
+            for(int j=0; j< amount+1; j++){
+
+                int take = (int)1e9;
+                if(j >= coins[i]){
+                    take = 1+dp[i][j-coins[i]];
+                }
+
+                int notake = dp[i-1][j];
+
+                dp[i][j] = Math.min(take,notake);
+            }
+        }
+
+        // int ans = f(0,coins,amount,dp);
+        int ans = dp[n-1][amount];
         if(ans == (int)1e9){
             return -1;
         }
